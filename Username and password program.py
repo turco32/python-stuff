eb6987e7037b2e685805aa7eb445de0fbfce7be8
Joseph Turco <italian.pepe.32@gmail.com>
2016-07-04 13:19:20 -0400

    Added a py file

---
+++ Username and password program.py
@@ -0,0 +1,11 @@
+print('hello, lets play a game!')
+name=input('what is the username?')
+if name == 'Mary':
+  print('Hello Mary')
+else:
+  print('Wrong Username')
+password=input('what is the password?')
+if password == 'swordfish':
+  print('access granted')
+else:
+  print ("Wrong Password")
